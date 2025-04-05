096a2b1e9d89d2aacfbc8570039051ea8798aef3
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2025-04-05 10:52:56 +0100

    Mejora menu de sucursal

--- sucursal.c
+++ sucursal.c
@@ -4,10 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void crea_sucursal (const char* ciudad, int capacidad){
-	char capacidad_str[10];
-    	sprintf(capacidad_str, "%d", capacidad);
-    	
+void crea_sucursal (const char* ciudad, char* capacidad){
 	// Generamos un duplicado del proceso actual
 	pid_t proceso = fork();
 	
@@ -18,13 +15,13 @@ void crea_sucursal (const char* ciudad, int capacidad){
 	}
 	// En caso contrario, se ejecuta "gnome-terminal" y se abre la aplicacion ./sala"
 	if(proceso == 0) {
-		execlp("gnome-terminal","gnome-terminal","--wait", "--", "./sala", ciudad, capacidad_str, NULL);
+		execlp("gnome-terminal","gnome-terminal","--wait", "--", "./sala", ciudad, capacidad, NULL);
 		printf("\nError al ejecutar gnome-terminal");
 		exit(-1);
 	}
 	int estado;
 	waitpid(proceso, &estado, 0);
-	printf("\nLa sala de %s ha cerrado", ciudad);
+	printf("La sala de %s ha cerrado\n\n", ciudad);
 	exit(0);
 }
 
@@ -34,18 +31,21 @@ int main(){
 	
 	// Generamos un bucle infinito que no termina hasta que se
 	// solicite salir del programa
+	printf("1. crea_sucursal <nombre> <capacidad>\n2. salir\n3. limpiar_panel\n");
+	char Instruccion[100];
 	while(1){
-		printf("\n¿Cual va a ser el nombre de la nueva sala?: ");
-		scanf("%s",nombresala); // Usamos %s para leer cadenas
-		
+		printf("\n");
+		scanf("%s",Instruccion); // Usamos %s para leer cadenas
+		if(!strcmp("crear_sucursal",Instruccion)){ // Creamos una sala.
+			char nombre[100];
+			char capacidad[100];
+			scanf("%s",&nombre);
+			scanf("%s",capacidad);
+			if(fork()==0) crea_sucursal(nombre,capacidad);
+		}
 		// Si el usuario escribe "salir" le saca del programa
-		if(!strcmp("salir",nombresala)) break;
-		
-		printf("\n¿Que capacidad va a tener la sala?: ");
-		scanf("%d",&capacidad); // Usamos %d para leer enteros
-		
-		if(fork()==0) crea_sucursal (nombresala, capacidad);
+		if(!strcmp("salir",Instruccion)) break;
 	}
-	printf("\nProceso finalizado");
+	printf("\nProceso finalizado\n");
 	return 0;
 }
